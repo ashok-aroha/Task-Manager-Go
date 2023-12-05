ccb4fc68aad1ba56482c841d2be7c13bc1f8a863
Ashok Mamnani <ashok.mamnani@ascentinfo.solutions>
2023-12-05 11:41:51 +0530

    First commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module Task-Manager-Go
+
+go 1.18
+
+require github.com/ashok-aroha/ACL-Go v1.0.3 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	a "github.com/ashok-aroha/ACL-Go"
 )
 
 // Handler function for a sample endpoint
@@ -12,7 +14,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	// Attach the middleware to the default ServeMux
-	http.Handle("/", restrictEndpoints(http.HandlerFunc(handler)))
+	http.Handle("/", a.RestrictEndpoints(http.HandlerFunc(handler)))
 
 	// Start the server on port 8080
 	fmt.Println("Server is running on http://localhost:8080")
